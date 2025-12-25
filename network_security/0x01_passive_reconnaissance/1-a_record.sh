6a037dd33a14ab184a5025da8f1f456baf4494db
Elcanc <elcan.ceferov.205@gmail.com>
2025-12-25 08:29:35 -0500

    x

--- network_security/0x01_passive_reconnaissance/1-a_record.sh
+++ network_security/0x01_passive_reconnaissance/1-a_record.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-  echo "Usage: $0 <domain>"
-  exit 1
-fi
-
-nslookup $1 | grep 'Address' | tail -n +2 | awk '{print $2}'
+nslookup $1 | awk '/^Address/ {print $2}'
