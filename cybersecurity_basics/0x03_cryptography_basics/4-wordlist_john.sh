3fa87a8d65137197b73fef9704bc4ade6dd57204
Elcanc <elcan.ceferov.205@gmail.com>
2026-02-02 04:02:03 -0500

    x

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=raw-md5 --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show --format=raw-md5 "$1" | cut -d: -f2 | sed '/^$/d' > 4-password.txt
+john --format=raw-md5 --wordlist=/usr/share/wordlists/rockyou.txt.gz "$1" && john --show --format=raw-md5 "$1" | cut -d: -f2 | sed 's/^/^/' > 4-password.txt
